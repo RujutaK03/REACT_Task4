4510a98657f95a7fd62bfab04faafbd4782a88cc
RujutaK03 <111700895+RujutaK03@users.noreply.github.com>
2023-10-16 21:08:40 +0530

    Update Task4.js

--- src/Tasks/Task4.js
+++ src/Tasks/Task4.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import "../../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "../../src/Tasks/Task4_styles.css"
 
 function Task4() {
